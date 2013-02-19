232874c155e4034bf72582decea8055c79fd43ea
mikelietz <codger+github@gmail.com>
2013-02-19 21:08:59 +0000

    Remove the loop since there's already just one $post. Middle stuff still needs to be fixed. Must redo that post navigation function!

--- single.php
+++ single.php
@@ -7,22 +7,19 @@
  * @since Twenty Thirteen 1.0
  */
 
-get_header(); ?>
+$theme->display('header'); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php echo $theme->content( $post ); ?>
 
-				<?php get_template_part( 'content', get_post_format() ); ?>
-				<?php twentythirteen_post_nav(); ?>
-				<?php comments_template(); ?>
+			<?php false && twentythirteen_post_nav(); ?>
+			<?php comments_template(); ?>
 
-			<?php endwhile; ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php $theme->display('sidebar'); ?>
+<?php $theme->display('footer'); ?>
