57a414eedcc34ecff3153ad65b694fc3176a6822
ringmaster <epithet@gmail.com>
2013-02-19 08:07:21 -0500

    Initial Commit

---
+++ header.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7 <?php echo $html_class; ?>">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8 <?php echo $html_class; ?>">
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width" />
+	<title><?php if($request->display_entry && isset($post)) { echo $post->title_title . ' - '; } ?><?php echo Options::get('title'); ?></title>
+	<meta name="generator" content="Habari">
+	<link rel="Shortcut Icon" href="<?php echo $theme->get_url('/favicon.png'); ?>">
+	<?php echo $theme->header(); ?>
+</head>
+
+<body <?php echo $theme->body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+			<hgroup>
+				<h1 class="site-title"><a href="<?php Site::out_url('habari'); ?>" title="<?php Options::out('title'); ?>" rel="home"><?php Options::out('title'); ?></a></h1>
+				<h2 class="site-description"><?php Options::out('tagline'); ?></h2>
+			</hgroup>
+
+			<div id="navbar" class="navbar">
+				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
+					<h3 class="menu-toggle"><?php _t( 'Menu', 'twentythirteen' ); ?></h3>
+					<a class="assistive-text skip-link" href="#content" title="<?php _t( 'Skip to content', 'twentythirteen' ); ?>"><?php _t( 'Skip to content', 'twentythirteen' ); ?></a>
+					<?php $theme->area('primary'); ?>
+					<?php /* get_search_form(); */ ?>
+				</nav><!-- #site-navigation -->
+			</div><!-- #navbar -->
+		</header><!-- #masthead -->
+
+		<div id="main" class="site-main">
---
+++ home.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+$theme->display('header'); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+		<?php if ( count($posts) > 0 ) : ?>
+
+			<?php /* The loop */ ?>
+			<?php foreach($posts as $post): ?>
+				<?php $theme->content($post); ?>
+			<?php endforeach; ?>
+
+			<?php twentythirteen_paging_nav(); ?>
+
+		<?php else : ?>
+			<?php $theme->display('content-none'); ?>
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php $theme->display('sidebar'); ?>
+<?php $theme->display('footer'); ?>
